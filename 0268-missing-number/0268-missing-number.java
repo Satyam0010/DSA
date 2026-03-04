48ea8dcbe9689491e8bc05e7958c6ff91d3fd0ac
Satyam <24110cn078@glbitm.ac.in>
2026-03-05 02:25:33 +0530

    Time: 0 ms (100%), Space: 47.1 MB (92.1%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum1 = 0,sum2=0;
+        for(int i = 0;i<=n;i++){
+            sum1 += i;
+        }
+        for(int j = 0;j<n;j++){
+            sum2 += nums[j];
+        }
+        return sum1 - sum2;
+    }
+}
