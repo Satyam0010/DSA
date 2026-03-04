a5518b49100f663d4e124449a69e38c86de94ab2
Satyam <24110cn078@glbitm.ac.in>
2026-03-05 02:27:28 +0530

    Time: 28 ms (42.85%), Space: 82.9 MB (74.04%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;   // find the middle index
+
+            // Divide
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Conquer (Merge the sorted halves)
+            merge(arr, left, mid, right);
+        }
+    }
+
+    void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        // Merge the temp arrays back into arr[left..right]
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        // Copy remaining elements if any
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
+}
