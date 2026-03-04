140fb2faee5aeb65b7e5302067ebbc4c20887afc
Satyam <24110cn078@glbitm.ac.in>
2026-03-05 02:22:51 +0530

    Time: 2103 ms (28%), Space: 42 MB (90.94%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num > 0) {
+            int sum = 0;
+            for (int i = 1; i < num ; i++)
+            {
+                if(num%i == 0)
+                  sum += i;
+            }
+            if(sum==num)
+             return true;
+             else
+             return false;
+        }
+            return false;
+    }
+}
