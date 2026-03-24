c261db5ed1997b42b6177ef87ede54eab448f7cf
Satyam <24110cn078@glbitm.ac.in>
2026-03-25 02:24:53 +0530

    Time: 8 ms (91.98%), Space: 46 MB (79.16%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-    int num = Integer.MAX_VALUE;
-    long pow = 1;
-    while(pow*3 <= num){
-        pow *= 3;
-    }
-    if(n > 0 && pow%n==0)
-    return true;
-
-
-    return false;
+        return n>0 && 1162261467%n == 0;
     }
 }
