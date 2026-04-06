82c1c944c8d57ce6f94e72c1dd85542eb282e4ba
Satyam <24110cn078@glbitm.ac.in>
2026-04-07 02:31:08 +0530

    Time: 0 ms (100%), Space: 43.8 MB (69.45%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int start = 0;
+        int end = n*m-1;
+
+
+        
+        while(start <= end){
+
+            int mid = start + (end - start)/2;
+            int row = mid/m;
+            int coloumn = mid%m;
+
+            int val = matrix[row][coloumn];
+
+            if(val == target){
+                return true;
+            } else if(val > target){
+                end = mid - 1;
+            } else {
+                start = mid+1;
+            }
+        } 
+        return false;
+    }
+}
