cbe81e749b99798f641055bcae46a2f87235f461
Satyam <24110cn078@glbitm.ac.in>
2026-06-01 13:16:31 +0530

    Time: 39 ms (69.23%), Space: 85.7 MB (95.83%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -2,11 +2,43 @@ class Solution {
     public int maximumGap(int[] nums) {
         int n = nums.length;
         if(n <= 1) return 0;
-        Arrays.sort(nums);
-        int gap = nums[1]-nums[0];
-        for(int i = 2; i < n;i++){
-            gap = Math.max(gap,Math.abs(nums[i]-nums[i-1]));
+        radixSort(nums);
+        int ans = 0;
+        for(int i = 1; i < n;i++){
+            ans = Math.max(ans,nums[i]-nums[i-1]);
         }
-        return gap;
+        return ans;
+    }
+    private void radixSort(int[] arr){
+        int max = 0;
+        for(int num : arr){
+            max = Math.max(max,num);
+        }
+        for(int exp = 1; max/exp > 0; exp*= 10){
+            countSort(arr,exp);
+        }
+    }
+    private void countSort(int[] arr,int exp){
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        for(int i = 0; i < n ; i++){
+            int digit = (arr[i]/exp)%10;
+            count[digit]++;
+        }
+
+        for(int i = 1; i < 10; i++){
+            count[i] += count[i-1];
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            int digit = (arr[i]/exp)%10;
+            output[count[digit]-1] = arr[i];
+            count[digit]--;
+        }
+
+        for(int i = 0; i < n ;i++){
+            arr[i] = output[i];
+        }
     }
 }
