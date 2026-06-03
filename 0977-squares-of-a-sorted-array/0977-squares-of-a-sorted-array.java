1eb4267f16a647cc6c2fcd76b94094996bca3979
Satyam <24110cn078@glbitm.ac.in>
2026-06-03 12:08:42 +0530

    Time: 8 ms (37.29%), Space: 47.3 MB (62.73%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -5,7 +5,32 @@ class Solution {
         for(int num : nums){
             ans[i++] = num*num;
         }
-        Arrays.sort(ans);
+        quickSort(ans,0,ans.length-1);
         return ans;
+    }
+    private void quickSort(int[] nums,int low,int high){
+        if(low >= high) return;
+        int s = low;
+        int e = high;
+        int m = s + (e - s)/2;
+        int p = nums[m];
+        while(s <= e){
+            while(nums[s] < p){
+            s++;
+            }
+            while(nums[e] > p){
+                e--;
+            }
+            if(s <= e){
+            int temp = nums[s];
+            nums[s]= nums[e];
+            nums[e]=temp;
+            s++;
+            e--;
+            }
+        }
+       
+        quickSort(nums,low,e);
+        quickSort(nums,s,high);
     }
 }
