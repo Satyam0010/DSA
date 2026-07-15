6b626bd6074d017834f6d0cf7cdc8e464c1fc328
Satyam <24110cn078@glbitm.ac.in>
2026-07-16 02:34:35 +0530

    Time: 2 ms (88.99%), Space: 47.9 MB (63.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int max = nums[0];
-        for(int i = 0; i < nums.length ; i++){
+        for(int i = 1; i < nums.length;i++){
             if(i > max) return false;
-            max = Math.max(max, i+nums[i]);
+            max = Math.max(max,i+nums[i]);
         }
         return true;
     }
