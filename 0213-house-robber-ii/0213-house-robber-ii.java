f8f14ce1ba035d35af047fd8a5cb773f82fbc256
Satyam <24110cn078@glbitm.ac.in>
2026-07-17 20:42:11 +0530

    Time: 0 ms (100%), Space: 42.6 MB (90.49%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,20 +2,13 @@ class Solution {
     public int rob(int[] nums) {
         int l = nums.length;
         if(l == 1) return nums[0];
-        int[] num1 = new int[l-1];
-        int[] num2 = new int[l-1];
-        for(int i = 0; i < l-1;i++){
-            num1[i] = nums[i+1];
-            num2[i] = nums[i];
-        }
-        return Math.max(check(num1),check(num2));
+        return Math.max(check(nums,1,l-1),check(nums,0,l-2));
     }
 
-    private int check(int[] nums){
-        int l = nums.length;
-        int prev = nums[0];
+    private int check(int[] nums,int start,int end){
+        int prev = nums[start];
         int prev2 = 0;
-        for(int i = 1; i < l; i++){
+        for(int i = start+1; i <= end; i++){
             int take = nums[i];
             if(i > 1) take += prev2;
             int nottake = 0+prev;
