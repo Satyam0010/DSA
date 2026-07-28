adea4fecfc6bab766926137ab71936d2f5b9ffa9
Satyam <24110cn078@glbitm.ac.in>
2026-07-29 01:09:42 +0530

    Time: 25 ms (75.6%), Space: 48 MB (88.4%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,28 +1,31 @@
 class Solution {
     public String smallestPalindrome(String s) {
         int l = s.length();
-        if(l == 1) return s;
-        TreeMap<Character,Integer> map = new TreeMap<>();
-        for(int i = 0; i < l;i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] ch = new int[26];
+        for(int i = 0; i< l/2;i++){
+            int idx = s.charAt(i)-'a';
+            ch[idx]++;
         }
-        StringBuilder front = new StringBuilder();
         char middle = '\0';
-        for(Map.Entry<Character,Integer> entry : map.entrySet()){
-            int freq = entry.getValue();
-            char ch = entry.getKey();
-            for(int i = 0; i < freq/2;i++){
-                front.append(ch);
-            }
-            if(freq%2 == 1) middle = ch;
+        if(l%2 == 1){
+            middle = s.charAt(l/2);
         }
-        StringBuilder ans = new StringBuilder();
-        ans.append(front);
+        int inn = 97;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 26;i++){
+            char c = (char) inn;
+            int ii = ch[i];
+            while(ii > 0){
+                sb.append(c);
+                ii--;
+            }
+            inn++;
+        }
+        StringBuilder ans = new StringBuilder(sb);
         if(middle != '\0'){
             ans.append(middle);
         }
-        ans.append(new StringBuilder (front).reverse());
+        ans.append(new StringBuilder(sb.reverse()));
         return ans.toString();
     }
 }
