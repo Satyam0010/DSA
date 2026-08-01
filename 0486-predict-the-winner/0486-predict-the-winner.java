35d6b882546d5d2560fbe6fafd144657fb144f02
Satyam <24110cn078@glbitm.ac.in>
2026-08-01 12:10:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (23.56%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,28 +1,17 @@
 class Solution {
-    int[][] dp = new int[20][20];
+    private Integer[][] dp = new Integer[20][20];
     public boolean predictTheWinner(int[] nums) {
         int len = nums.length;
         if(len == 1) return true;
-        int total = 0;
-        for(int i = 0; i < 20;i++){
-            for(int j = 0; j < 20;j++){
-                dp[i][j] = -1;
-            }
-        }
-        for(int num : nums){
-            total += num;
-        }
-        int player1 = solve(0,len-1,nums);
-        int player2 = total - player1;
-        return player1 >= player2;
+        return solve(0,len-1,nums) >= 0;
     }
 
     private int solve(int i , int j,int[] nums){
         if(i > j) return 0;
         if(i == j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        int take_i = nums[i] + Math.min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int take_j = nums[j] + Math.min(solve(i,j-2,nums),solve(i+1,j-1,nums));
+        if(dp[i][j] != null) return dp[i][j];
+        int take_i = nums[i] - solve(i+1,j,nums);
+        int take_j = nums[j] - solve(i,j-1,nums);
         return dp[i][j] = Math.max(take_i,take_j);
     }
 }
