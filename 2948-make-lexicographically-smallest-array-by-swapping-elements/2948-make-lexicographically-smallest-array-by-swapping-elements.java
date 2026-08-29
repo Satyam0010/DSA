6ca2f7366465d4ff8ce13cf0d7890739f151dfae
Satyam <24110cn078@glbitm.ac.in>
2026-08-29 15:05:13 +0530

    Time: 77 ms (81.82%), Space: 156.8 MB (66.67%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,31 +1,34 @@
 class Solution {
-    class Pair{
+    class Pair {
         int value;
         int index;
-        Pair(int value,int index){
+
+        Pair(int value, int index) {
             this.value = value;
             this.index = index;
         }
     }
+
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
         List<Pair> list = new ArrayList<>();
-        for(int i = 0 ; i < nums.length;i++){
-            list.add(new Pair(nums[i],i));
+        for (int i = 0; i < nums.length; i++) {
+            list.add(new Pair(nums[i], i));
         }
-        list.sort((a,b) -> Integer.compare(a.value,b.value));
+        list.sort((a, b) -> Integer.compare(a.value, b.value));
         int start = 0;
-        while(start < list.size()){
+        while (start < list.size()) {
             int end = start;
-            while(end+1 < list.size() && list.get(end+1).value - list.get(end).value <= limit) end++;
+            while (end + 1 < list.size() && list.get(end + 1).value - list.get(end).value <= limit)
+                end++;
             List<Integer> indices = new ArrayList<>();
-            for(int i = start;i <= end;i++){
+            for (int i = start; i <= end; i++) {
                 indices.add(list.get(i).index);
             }
             Collections.sort(indices);
-            for(int i = 0; i < indices.size();i++){
-                nums[indices.get(i)] = list.get(start+i).value;
+            for (int i = 0; i < indices.size(); i++) {
+                nums[indices.get(i)] = list.get(start + i).value;
             }
-            start = end+1;
+            start = end + 1;
         }
         return nums;
     }
