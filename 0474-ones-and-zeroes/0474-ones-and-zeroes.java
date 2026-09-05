44567fa9b11fc06c1bc1f29927aa9ffdebf09d57
Satyam <24110cn078@glbitm.ac.in>
2026-09-05 13:32:10 +0530

    Time: 31 ms (49.98%), Space: 88 MB (24.3%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,20 +1,32 @@
 class Solution {
+    private Integer[][][] memo;
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][] memo = new int[m+1][n+1];
+        if(strs == null || strs.length == 0) return 0;
+        int[][] count = new int[strs.length][2];
+        memo = new Integer[m+1][n+1][strs.length];
+        int i = 0;
         for(String s : strs){
-            int zeroes = 0;
-            int ones = 0;
             char[] ch = s.toCharArray();
+            int zero = 0, one = 0;
             for(char c : ch){
-                if(c == '0') zeroes++;
-                else ones++;
+                if(c == '0') zero++;
+                else one++;
             }
-            for(int i = m; i >= zeroes;i--){
-                for(int j = n; j >= ones;j--){
-                    memo[i][j] = Math.max(memo[i][j],memo[i-zeroes][j-ones]+1);
-                }
-            }
+            count[i][0] = zero;
+            count[i][1] = one;
+            i++;
         }
-        return memo[m][n];
+        return check(count,m,n,strs.length-1);
+    }
+
+    private int check(int[][] count,int m, int n,int index){
+        if(index < 0 ||( m < 0 && n < 0)) return 0;
+        if(memo[m][n][index] != null) return memo[m][n][index];
+        int skip = check(count,m,n,index-1);
+        int take = 0;
+        if(count[index][0] <= m && count[index][1] <= n){
+            take = 1 + check(count,m-count[index][0],n-count[index][1],index-1);
+        }
+        return memo[m][n][index] = Math.max(take,skip); 
     }
 }
