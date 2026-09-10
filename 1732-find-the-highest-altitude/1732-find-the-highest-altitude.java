821a0ca017a333f92ae0971ad609affb28c6e2f9
Satyam <24110cn078@glbitm.ac.in>
2026-09-10 21:06:24 +0530

    Time: 0 ms (100%), Space: 43.2 MB (38.03%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n = gain.length;
+        int[] ans = new int[n+1];
+        ans[0] = 0;
+        for(int i = 0 ; i < n;i++){
+            ans[i+1] = ans[i]+gain[i];
+        }
+        int max = Integer.MIN_VALUE;
+        for(int num : ans){
+            if(num > max) max = num;
+        }
+        return max;
+    }
+}
