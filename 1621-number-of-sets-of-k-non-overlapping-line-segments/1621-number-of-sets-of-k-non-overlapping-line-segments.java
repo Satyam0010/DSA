72f0a5f294356c470d502b4cecf8e5aa3e633cd6
Satyam <24110cn078@glbitm.ac.in>
2026-09-16 22:03:15 +0530

    Time: 19 ms (81.94%), Space: 42.2 MB (84.52%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,21 +1,15 @@
 class Solution {
-    private static final int MOD = 1_000_000_007;
     public int numberOfSets(int n, int k) {
-        int tab[][] = new int[k+1][n+1];
-        for(int i = 0; i <= n; i++){
-            tab[0][i] = 1;
-        }
-        for(int segment = 1; segment <= k; segment++){
-            int[] prevSum = new int[n+1];
-            for(int position = n-1 ; position >= 0 ; position--){
-                prevSum[position] = (int) ((prevSum[position+1] + tab[segment-1][position])%MOD);
-            }
-            for(int position = n-1; position >= 0; position--){
-                long ways = tab[segment][position+1];
-                ways += prevSum[position+1];
-                tab[segment][position] = (int) (ways%MOD);
+        int MOD = 1_000_000_007;
+        long dp[] = new long[k+1];
+        long sum[] = new long[k+1];
+        dp[0] = 1;
+        for(int i = 1; i < n; i++){
+            for(int j = k; j >= 1;j--){
+                sum[j] = (sum[j] + dp[j-1])%MOD;
+                dp[j] = (dp[j] + sum[j])%MOD;
             }
         }
-        return tab[k][0];
+        return (int)dp[k];
     }
 }
