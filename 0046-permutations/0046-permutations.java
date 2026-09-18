ce199e106e013c9a33beaaaacd0b1730fd129253
Satyam <24110cn078@glbitm.ac.in>
2026-09-18 16:05:17 +0530

    Time: 1 ms (88.01%), Space: 45.4 MB (62.59%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        check(nums,new ArrayList<>(),used,result);
+        return result;
+    }
+
+    private void check(int[] nums, List<Integer> current,boolean[] used,List<List<Integer>> result){
+        if(current.size() == nums.length){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for(int i = 0; i < nums.length ; i++){
+            if(used[i] == true) continue;
+            current.add(nums[i]);
+            used[i] = true;
+            check(nums,current,used,result);
+            current.remove(current.size()-1);
+            used[i] = false;
+        }
+    }
+}
